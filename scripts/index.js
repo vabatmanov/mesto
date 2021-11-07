a20f1ce521ad7bee9da70b1105c72b989f03964a
Batmanov Vlad <v-batmanov@yandex.ru>
2021-11-08 02:17:30 +0700

    fix v1.9

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,3 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const validDate = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -39,11 +12,14 @@ const listCards = document.querySelector('.cards');
 
 // Popup
 const popup = document.querySelectorAll('.popup');
+
+const popupProfileEdit = document.querySelector('.popup_profile-edit');
+
+const popupCardAdd = document.querySelector('.popup_card-add');
+
+const popupCardOpen = document.querySelector('.popup_card-open');
 const popupImage = document.querySelector('.popup__image');
 const popupNameImage = document.querySelector('.popup__name-image');
-const popupProfileEdit = document.querySelector('.popup_profile-edit');
-const popupCardAdd = document.querySelector('.popup_card-add');
-const popupCardOpen = document.querySelector('.popup_card-open');
 
 //Button open
 const editButton = document.querySelector('.profile__button-edit');
@@ -66,6 +42,121 @@ const inputCardLink = document.querySelector('.popup__input_contains_card-link')
 
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
+
+//Открыть popup
+function openPopup(elementPopup) {
+  elementPopup.classList.add('popup_opened');
+  elementPopup.addEventListener('click', closeOverlayPopup);
+  document.addEventListener('keydown', closeEscapePopup);
+}
+
+//Закрыть popup
+function closePopup(elementPopup) {
+  elementPopup.classList.remove('popup_opened');
+  elementPopup.removeEventListener('click', closeOverlayPopup);
+  document.removeEventListener('keydown', closeEscapePopup);
+}
+
+//Закрыть при клике на оверлей
+function closeOverlayPopup(element) {
+  if (element.target.classList.contains('popup')) {
+    closePopup(element.target);
+  }
+}
+
+//Закрыть при нажатии на Escape
+function closeEscapePopup(element) {
+  if (element.key === "Escape"){
+    closePopup(document.querySelector('.popup_opened'));
+  }
+}
+
+//Функция отправки формы
+function submitFormEdit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputHobbies.value;
+  closePopup(popupProfileEdit);
+}
+
+function submitFormAdd(event) {
+  event.preventDefault();
+  createCards ({
+    name: inputCardName.value,
+    link: inputCardLink.value
+  });
+  closePopup(popupCardAdd);
+}
+
+//добавить карту
+function addCard(card) {
+  listCards.prepend(card);
+}
+
+/* Сформировать карту
+Принимает объект {name,link} или массив объектов [{name,link},{name,link},...] */
+function createCards(item) {
+  if (Array.isArray(item)) {
+    item.forEach(function (objectCard) {
+      addCard(createFormCard(objectCard));
+    })
+  } else {
+    addCard(createFormCard(item));
+  }
+
+  function createFormCard(card) {
+    const templateCardItem = templateCard.querySelector('.cards__item').cloneNode(true);
+    templateCardItem.querySelector('.cards__image').alt = 'Изображение ' + card.name;
+    templateCardItem.querySelector('.cards__image').src = card.link;
+    templateCardItem.querySelector('.cards__caption').textContent = card.name;
+    templateCardItem.querySelector('.cards__image').addEventListener('click', function (evt) {
+      popupImage.src = evt.target.src;
+      popupImage.alt = evt.target.alt;
+      popupNameImage.textContent = (evt.target.parentElement).querySelector('.cards__caption').textContent;   //evt.target.nextElementSibling.textContent;
+      openPopup(popupCardOpen);
+    })
+    templateCardItem.querySelector('.cards__bin').addEventListener('click', function (evt) {
+      evt.target.closest('.cards__item').remove();
+    });
+    templateCardItem.querySelector('.cards__like').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('cards__like_enable');
+    });
+    return templateCardItem;
+  }
+}
+
+//События кнопок закрыть
+editButtonClose.addEventListener('click', () => closePopup(popupProfileEdit));
+addButtonClose.addEventListener('click', () => closePopup(popupCardAdd));
+openButtonClose.addEventListener('click', () => closePopup(popupCardOpen));
+
+//Событие открытия окна редактирования профиля
+editButton.addEventListener('click', () => {
+  inputName.value = profileName.textContent;
+  inputHobbies.value = profileDescription.textContent;
+  clearFormValidation(popupProfileEdit, validDate);
+  openPopup(popupProfileEdit);
+});
+
+//Событие открытия окна добавление карточки
+addButton.addEventListener('click', () => {
+  popupFormAdd.reset();
+  clearFormValidation(popupCardAdd, validDate);
+  openPopup(popupCardAdd)
+});
+
+//События отправки форм
+popupFormEdit.addEventListener('submit', submitFormEdit);
+popupFormAdd.addEventListener('submit', submitFormAdd);
+
+
+//Инициализация Шесть карточек «из коробки»
+createCards(initialCards);
+enableValidation(validDate);
+
+
+
+/*
 
 function closePopupHandle(event) {
   if (event.type === "mousedown") {
@@ -114,61 +205,28 @@ function submitFormAdd(event) {
   togglePopup(popupCardAdd);
 }
 
-//добавить карту
-function addCard(card) {
-  listCards.prepend(card);
-}
 
-/* Сформировать карту
-Принимает объект {name,link} или массив объектов [{name,link},{name,link},...] */
-function createCards(item) {
-  if (Array.isArray(item)) {
-    item.forEach(function (objectCard) {
-      addCard(createFormCard(objectCard));
-    })
-  } else {
-    addCard(createFormCard(item));
-  }
 
-  function createFormCard(card) {
-    const templateCardItem = templateCard.querySelector('.cards__item').cloneNode(true);
-    templateCardItem.querySelector('.cards__image').alt = 'Изображение ' + card.name;
-    templateCardItem.querySelector('.cards__image').src = card.link;
-    templateCardItem.querySelector('.cards__caption').textContent = card.name;
-    templateCardItem.querySelector('.cards__image').addEventListener('click', function (evt) {
-      popupImage.src = evt.target.src;
-      popupImage.alt = evt.target.alt;
-      popupNameImage.textContent = evt.target.nextElementSibling.textContent;
-      togglePopup(popupCardOpen);
-    })
-    templateCardItem.querySelector('.cards__bin').addEventListener('click', function (evt) {
-      evt.target.closest('.cards__item').remove();
-    });
-    templateCardItem.querySelector('.cards__like').addEventListener('click', function (evt) {
-      evt.target.classList.toggle('cards__like_enable');
-    });
-    return templateCardItem;
-  }
-}
 
-popup.forEach((item) => {
-  item.addEventListener('mousedown', closePopupHandle);
-  document.addEventListener('keydown', closePopupHandle);
-});
+
+
+
+
+
+
+
 
 popupFormEdit.addEventListener('submit', submitFormEdit);
 popupFormAdd.addEventListener('submit', submitFormAdd);
+
 editButton.addEventListener('click', () => {
   inputName.value = profileName.textContent;
   inputHobbies.value = profileDescription.textContent;
   togglePopup(popupProfileEdit);
 });
 addButton.addEventListener('click', () => togglePopup(popupCardAdd));
+
 editButtonClose.addEventListener('click', () => togglePopup(popupProfileEdit));
 addButtonClose.addEventListener('click', () => togglePopup(popupCardAdd));
 openButtonClose.addEventListener('click', () => togglePopup(popupCardOpen));
-
-//Инициализация Шесть карточек «из коробки»
-createCards(initialCards);
-enableValidation(validDate);
-
+ */
